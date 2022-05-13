9efee59ecce922d767cdf6e474ca37e385ed7e8c
Julia Kamischke <julia@Julias-MacBook-Pro.local>
2022-05-13 10:39:29 +0200

    first working version of zipper

---
+++ zipper_function.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 10:28:40 2022
+
+@author: julia
+"""
+
+food = ['pizza', 'sushi', 'jam']
+people = ['Bob', 'Kevin', 'Angela']
+
+def zipper(listA, listB):
+    zipped = zip(listA, listB)
+    
+    return zipped
+
+if __name__ == '__main__': # run directly the function
+    
+    food = ['pizza', 'sushi', 'jam']
+    people = ['Bob', 'Kevin', 'Angela']
+    
+    result = zipper(food, people)
+    for item in result:
+        print(item)
